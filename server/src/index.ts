8e7b4d6e5afa3770efaed766713aa03496c4b8c9
Stuart Huggins <stuhuggins@gmail.com>
2021-11-10 21:04:25 +0000

    complete module 4

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,23 @@
-import express from 'express';
+require('dotenv').config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql';
+import { connectDatabase } from './database';
 
-const app = express();
-const port = 9000;
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: () => ({ db })
+    });
+    await server.start();
+    server.applyMiddleware({ app, path: '/api' });
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.start().then(() => server.applyMiddleware({ app, path: '/api' }));
+    app.listen(process.env.PORT);
 
-app.listen(port);
+    console.log(`[app]: http://localhost:${process.env.PORT}`);
+};
 
-console.log(`[app]: http://localhost:${port}`);
+mount(express());
