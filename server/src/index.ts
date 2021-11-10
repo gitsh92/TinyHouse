bcbaf08579b2e5eb4601ba36ed37c4455c8723b0
Stuart Huggins <stuhuggins@gmail.com>
2021-11-10 19:16:14 +0000

    complete module 3

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-app.get('/listings', (_req, res) => {
-    return res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-    const id: string = req.body.id;
-
-    for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-            return res.send(listings.splice(i, 1)[0]);
-        }
-    }
-
-    res.send('Failed to delete listing.');
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+server.start().then(() => server.applyMiddleware({ app, path: '/api' }));
 
 app.listen(port);
 
