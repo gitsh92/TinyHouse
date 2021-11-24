d05a3a83d6c397d581a896dcc822b4d0f5a4be36
Stuart Huggins <stuhuggins@gmail.com>
2021-11-24 22:17:10 +0000

    complete module 14

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-import { Database } from '../lib/types';
+import { Booking, Database, Listing, User } from '../lib/types';
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.ukzuz.mongodb.net/main?retryWrites=true&w=majority`;
 
@@ -8,6 +8,8 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db('main');
 
   return {
-    listings: db.collection('test_listings')
+    bookings: db.collection<Booking>('bookings'),
+    listings: db.collection<Listing>('listings'),
+    users: db.collection<User>('users')
   };
 };
